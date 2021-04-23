cedccefc994fc992b0bdbce2e3f438684a19619a
Zankrut Goyani <68992386+zankrut20@users.noreply.github.com>
2021-04-23 18:23:08 +0530

    ready for CI and upload

--- tests/testthat/test-comb.indices.R
+++ tests/testthat/test-comb.indices.R
@@ -1,6 +1,6 @@
 test_that("Whether result is same as output", {
   gmat<- gen.varcov(seldata[,3:9], seldata[,2], seldata[,1])
   pmat<- phen.varcov(seldata[,3:9], seldata[,2], seldata[,1])
-  cindex<- comb.indices(ncomb = 1, phen_mat = pmat, gen_mat = gmat, weight_mat = weight[,-1], weight_col = 1)
+  cindex<- comb.indices(ncomb = 1, pmat = pmat, gmat = gmat, wmat = weight[,-1], wcol = 1)
   expect_equal(nrow(cindex),7)
 })
