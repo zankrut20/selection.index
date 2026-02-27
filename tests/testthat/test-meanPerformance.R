fd9d67f3303da28bfddda1f3d376088c410ba44e
Zankrut Goyani <zankrut20@gmail.com>
2026-02-28 01:53:49 +0530

    fix: delete un-tracked legacy test-meanPerformance.R causing R CMD check failure

--- tests/testthat/test-meanPerformance.R
+++
@@ -1,4 +0,0 @@
-test_that("Whether result is same as output", {
-  performance <- d <- meanPerformance(data = seldata[,3:7], genotypes = seldata[,2], replications = seldata[,1])
-  expect_equal(nrow(performance),34)
-})
