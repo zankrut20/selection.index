6253fc0e9edb3258b683433d9a99488c726f8183
Zankrut Goyani <zankrut20@gmail.com>
2026-02-28 01:54:44 +0530

    fix: delete remaining legacy test files calling deleted functions

--- tests/testthat/test-comb.indices.R
+++
@@ -1,6 +0,0 @@
-test_that("Whether result is same as output", {
-  gmat<- gen.varcov(seldata[,3:9], seldata[,2], seldata[,1])
-  pmat<- phen.varcov(seldata[,3:9], seldata[,2], seldata[,1])
-  cindex<- comb.indices(ncomb = 1, pmat = pmat, gmat = gmat, wmat = weight[,-1], wcol = 1)
-  expect_equal(nrow(cindex),7)
-})
--- tests/testthat/test-gen.advance.R
+++
@@ -1,9 +0,0 @@
-test_that("Whether gen.advance gives same output", {
-  seldata<- rnorm(2,3,4)
-  gmat<- matrix(1:16, nrow = 4)
-  pmat<- matrix(1:16, nrow = 4)
-  weight<- matrix(1.00)
-  GA<- round(gen.advance(phen_mat = pmat[1,1], gen_mat = gmat[1,1], weight_mat = weight),2)
-  result<- structure(2.060, .Dim = c(1L, 1L))
-  expect_equal(result, GA)
-})
